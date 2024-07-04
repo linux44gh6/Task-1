9a95b27008db5b32dc2e0ee3c7119dd9f4589d90
linuxj4j <abireshan32@gail.com>
2024-07-04 08:29:45 +0600

    add responsive code

--- src/Components/Care/Care.jsx
+++ src/Components/Care/Care.jsx
@@ -8,19 +8,19 @@ import star from '../../assets/fi-sr-star.svg'
 const Care = () => {
     return (
         <div className=''>
-            <h1 className=' text-center font-font1 text-5xl w-[532px] mx-auto font-bold mt-10'>Comprehensive Care for Every Patient</h1>
-           <div className=' grid grid-cols-5 gap-5 -mt-16'>
-           <div className='w-[216px] bg-color3 p-3 rounded-2xl border h-[312px] space-y-4'>
+            <h1 className=' text-center font-font1 text-2xl md:text-text-5xl lg:text-5xl w-full md:w-[532px] lg:w-[532px] mx-auto font-bold mt-10'>Comprehensive Care for Every Patient</h1>
+           <div className=' grid grid-cols-1 md:grid-cols-5 lg:grid-cols-5 gap-1 md:gap-5 lg:gap-5 lg:-mt-16'>
+           <div className=' w-full md:w-[216px] lg:w-[216px] bg-color3 p-3 rounded-2xl border h-[312px] space-y-4'>
                 <h1 className=' text-4xl font-font1 font-bold'>90%</h1>
                 <p className=' font-font1 text[20px] w-[156px]'>Patient satisfaction rate, reflecting our commitment</p>
                 <img className=' translate-x-10 w-32' src={chart} alt="" />
             </div>
-            <div className='w-[216px] bg-color3 p-3 rounded-2xl border h-[190px] mt-[121px] space-y-2'>
+            <div className='w-full md:w-[216px] lg:w-[216px] bg-color3 p-3 rounded-2xl border h-[190px] mt-3 md:mt-[121px] lg:mt-[121px] space-y-2'>
                 <h1 className=' text-4xl font-font1 font-bold'>500+</h1>
                 <p className='font-font1 w-[115px]'>Board-certified doctors</p>
                 <img className=' translate-x-24 -translate-y-3' src={contract} alt="" />
             </div>
-            <div className='w-[216px] bg-[#FBFBFB] p-3 rounded-2xl border h-[162px] mt-[149px] space-y-2'>
+            <div className='w-full md:w-[216px] lg:w-[216px] bg-[#FBFBFB] p-3 rounded-2xl border h-[162px] mt-3 md:mt-[149px] lg:mt-[149px] space-y-2'>
                 <h1 className='flex items-center gap-5 text-5xl font-bold '>4.8 <img className=' w-10' src={star} alt="" /></h1>
                 <p className='font-font1'>Over 20,000 Patient</p>
                 <div className='flex'>
@@ -30,13 +30,13 @@ const Care = () => {
                 <img src={Ellipse} alt="" />
                 </div>
             </div>
-            <div className='w-[216px] bg-color3 p-3 rounded-2xl border h-[190px] mt-[121px] space-y-2'>
+            <div className='w-full md:w-[216px] lg:w-[216px] bg-color3 p-3 rounded-2xl border h-[190px] mt-3 md:mt-[121px] lg:mt-[121px] space-y-2'>
                 <h1 className=' text-4xl font-font1 font-bold'>$5000</h1>
                 <p className=' font-font1 w-[115px]'>Money spend
                 for poor patient</p>
                 <img className=' translate-x-20 -translate-y-2' src={coin} alt="" />
             </div>
-            <div className='w-[216px] bg-color3 p-3 rounded-2xl border h-[316px] space-y-2 '>
+            <div className='w-full md:w-[216px] lg:w-[216px] bg-color3 p-3 rounded-2xl border h-[316px] space-y-2'>
                 <h1 className=' text-4xl font-font1 font-bold'>50+</h1>
                 <p className=' font-font1 w-[156px]'>Free lession video 
                 for patient</p>
--- src/Components/Solution/Solution.jsx
+++ src/Components/Solution/Solution.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { FiArrowUpRight } from "react-icons/fi";
 const Solution = () => {
     return (
-        <div className='flex justify-between mt-40'>
-          <div className='w-[499px] '>
+        <div className='flex flex-col md:flex-row lg:flex-row justify-between mt-10 md:mt-40 lg:mt-40'>
+          <div className=' w-full md:w-[499px] lg:w-[499px] '>
             <button className=' btn bg-[#FFFFF5] rounded-full px-14'>Who we are</button>
             <h1 className=' text-5xl font-font1 font-bold mt-2'>We Help To Get Soultions</h1>
             <p className='font-font1 mt-4'>We are proud to be a trusted healthcare provider in our community, and we look forward to serving you and your family with excellence, integrity, and compassion. Your health is our priority, and we are here for you every step of the way. We believe in treating each patient with dignity, respect, and empathy, ensuring that they receive the attention and care they deserve.
@@ -14,7 +14,7 @@ const Solution = () => {
            <div className=' relative'>
            <img className=' w-[518px] h-[315px]' src="https://i.ibb.co/Dk8kDHx/Rectangle-24.png" alt="" />
            </div>
-            <div className=' w-[380px] bg-[#343268] text-white  px-4 py-10 rounded-2xl absolute -translate-y-28 -translate-x-32'>
+            <div className=' w-full md:w-[380px] lg:w-[380px] bg-[#343268] text-white  px-4 py-10 rounded-2xl md:absolute lg:absolute md:-translate-y-28 lg:-translate-y-28 md:-translate-x-32 lg:-translate-x-32'>
                 <h1 className=' font-font1 text-4xl'>Our mission is simple</h1>
                 <p className='font-font1 w-[374px]'>To provide high-quality healthcare services that are accessible, personalized, and patient-centered.</p>
             </div>
